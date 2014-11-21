e575a5ae4493fdb562f45a9b17fab0ab66f80523
Andrey Danilchyk <andrey.danilchyk@gmail.com>
2014-11-21 23:54:00 +0300

    Add All tab to open prs by component

--- static/js/controllers/PRController.js
+++ static/js/controllers/PRController.js
@@ -8,7 +8,7 @@ define([
   function($, _, React, SubnavView, PRTableView) {
   // jscs:enable
   var Controller = function(prURL, navbarDomContainer, prTableDomContainer) {
-    this.selectedComponent = "Docs";
+    this.selectedComponent = "All";
     this.prURL = prURL;
     this.navbarDomContainer = navbarDomContainer;
     this.prTableDomContainer = prTableDomContainer;
@@ -24,34 +24,35 @@ define([
     // The response is a flat list of pull requests.
     // Each pull request may be associated with multiple components, so flatMap them out:
     var flattenedwithComponents = _.flatten(_.map(this.prs, function(pr) {
-      return _.map(pr.components, function(component) {
+      var arr = _.map(pr.components, function(component) {
         return {component: component, pr: pr}
       });
+
+      arr.push({component: "All", pr: pr});
+      return arr;
     }));
+
     // Group by component:
     var groupedByComponent = _.groupBy(flattenedwithComponents, function(x) {
       return x.component;
     });
     // Map the group values so that we're left with a map from component names
     // to pull requests in those components
-    var groups = _.object(_.map(groupedByComponent, function(groupValues, component) {
+    var groups = _.map(groupedByComponent, function(groupValues, component) {
       return [component, _.map(groupValues, function(v) {
         return v.pr
       })];
-    }));
-
-    var navElems = _.map(groups, function(groupValues, component) {
-      return {label: component + " (" + groupValues.length + ")", key: component};
     });
 
     React.render(
       React.createElement(SubnavView, {
-        elems: navElems,
+        elems: groups,
         active: this.selectedComponent,
         onSelect: this.onSelectComponent.bind(this)}),
       this.navbarDomContainer
     );
 
+    groups = _.object(groups);
     React.render(
       React.createElement(PRTableView, {prs: groups[this.selectedComponent]}),
       this.prTableDomContainer
--- static/jsx/views/SubnavView.jsx
+++ static/jsx/views/SubnavView.jsx
@@ -27,26 +27,39 @@ define([
 
     var SubnavView = React.createClass({
       propTypes: {
-        elems: React.PropTypes.arrayOf(React.PropTypes.shape({
-          label: React.PropTypes.string.isRequired,
-          key: React.PropTypes.string.isRequired
-        })),
+        elems: React.PropTypes.array,
         active: React.PropTypes.string.isRequired,
         onSelect: React.PropTypes.func.isRequired
+      },
+      componentDidMount: function() {
+        this.props.onSelect("All");
       },
       onSelect: function(key) {
         this.props.onSelect(key);
       },
       render: function() {
         var outer = this;
-        var navElements = _.map(this.props.elems, function(e) {
-          var boundClick = outer.onSelect.bind(outer, e.key);
-          return (<NavEntry
-            label={e.label}
-            key={e.key}
-            onClick={boundClick}
-            active={e.key == outer.props.active}/>);
+        var navElements = [];
+
+        //sort by count of prs
+        var elems = this.props.elems;
+        elems.sort(function(arr1, arr2) {
+          return _.last(arr2).length - _.last(arr1).length;
         });
+
+        for (var i = 0; i < elems.length; i++) {
+          var component = _.first(elems[i]);
+          var prsLength = _.last(elems[i]).length;
+          var boundClick = outer.onSelect.bind(outer, component);
+          var label = component + " (" + prsLength + ")";
+
+          navElements.push(<NavEntry
+            label={label}
+            key={component}
+            onClick={boundClick}
+            active={component == outer.props.active}/>);
+        }
+
         return (
           <nav className="sub-nav navbar navbar-default"
             role="navigation">
